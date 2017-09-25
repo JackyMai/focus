396e88d170d2d164e386834dd1d521554aef44fa
Jacky Mai <jacky.jy.mai@gmail.com>
2017-09-26 02:39:02 +1300

    Popup script now sends message to background script to play/pause audio

--- js/work-break.js
+++ js/work-break.js
@@ -7,18 +7,17 @@ function toggleCycle() {
     }
 }
 
-function togglePlay() {
-    var rain = document.getElementById('rain-audio');
-    if (rain.paused) {
-        rain.play();
-    } else {
-        rain.pause();
-    }
+function togglePlay(event) {
+    var targetID = event.target.id;
+    chrome.runtime.sendMessage({audioID: targetID}, function(response) {
+        console.log(response.farewell);
+    });
 }
 
 function attachListeners() {
     document.getElementById('cycle-btn').addEventListener('click', toggleCycle);
-    document.getElementById('ambient-rain').addEventListener('click', togglePlay);
+    document.getElementById('ambient-storm').addEventListener('click', togglePlay);
+    document.getElementById('ambient-birds').addEventListener('click', togglePlay);
 }
 
 window.addEventListener('load', attachListeners);
